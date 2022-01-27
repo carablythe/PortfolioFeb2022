427d92234946f16824e659efe075da20f64a2b79
Cara Phillips <caraphillips@Caras-MacBook-Air-2.local>
2022-01-28 04:13:58 +0900

    dropdown menus created

---
+++ app.js
@@ -0,0 +1,27 @@
+// $(() => {
+//   $('.about').on('click', () => {
+//     $('.aboutUL').attr("display", "")
+//     $('.aboutUL').attr("pointer-events", "all")
+//     $('.aboutUL').attr("transform", "translateY(0px)")
+//   }
+// )})
+
+// //
+// $(() => {
+//     $('.about').hover(function(){
+//       $('.aboutUL',this).slideDown(500)
+//       }
+//       function(){
+//       $('.aboutUL',this).slideUp(300)
+//       }
+//   })
+
+  //
+  // $(() => {
+  //   if $('.about').on('click') {
+  //     $('.aboutUL').show('slow')
+  //   }
+  //   else {
+  //     $('.aboutUL').hide('slow')
+  //   }
+  // )})
