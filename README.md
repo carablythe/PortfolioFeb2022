68aafee96f4644b88e027871f8a8b863df2e9353
Cara Phillips <caraphillips@Caras-MacBook-Air-2.local>
2022-02-01 07:00:12 +0900

    got at least 3 of my interactions to be javascript based

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # PortfolioFeb2022
 SEI project portfolio February 2022
 
-
 PORTFOLIO SITE: https://carasensei.com
-
 
 OVERALL APPROACH TAKEN:
 2000s style somewhat professional homepage.
@@ -15,9 +13,11 @@ UNSOLVED PROBLEMS:
 
 My layout is a little wobbly and not the greatest in appearance. I ended up doing a media query for a minimum width of around 900px because it looked good on a computer and iPhone screen with a min-width of 500px, but not the tablets.  Maybe there is a better way to do it?
 
-Also, although I spent many hours playing with various color palettes, layouts, images, etc., I am not satisfied with the general appearance at all. It looks so simple and not very professional...a total "newb" vibe going on.
-
 While I eventually figured out how to do dropdown menus using CSS, I first tried to learn various ways of making them via javascript and jQuery, at which I did not succeed, but would someday soon like to learn how to do. (I left my *work-that-didn't-work* commented out at the bottom of my app.js file for your reference).
+
+UPDATE on DROP-DOWNs: It's Monday and I realized they don't work in Safari...I have only check Firefox and Chrome until now, so am very disappointed/frustrated to realize this on the last day.
+
+Also, although I spent many hours playing with various color palettes, layouts, images, etc., I am not satisfied with the general appearance at all. It looks so simple and not very professional...a total "newb" vibe going on.
 
 Finally, not a problem, just a point of lack of completeness, but the content was kind of thrown together due to lack of time.  My resume, in particular, is very rough.
 
--- app.js
+++ app.js
@@ -9,75 +9,93 @@ const readMeText = () => {
 }
 
 
-/* The below is the set-up for my contact page submit alert: */
+/* The below is the set-up for my contact page submit message: */
 
-$(document).ready(function(){
-  $(".submit").click(function(){
-    alert("Thank you for contacting me! I look forward to reading your message and will be in touch soon. Have a great day!");
-  });
-});
-
+$(() => {
+  $('.submit').on('click', () => {
+    $('.imResponse').text("Thank you for contacting me! I look forward to reading your message and will be in touch soon. Have a great day!   - Cara")})
+})
 
 
-// The below code was an attempt to get the "read me" button to work on the Game Space page...Ashley helped me get it working with the first code on this page....
+//hover effect on pics
+
+$(document).ready(function() {
+    $('.first_game').mouseenter(function() {
+    $('.first_game').css("background-image", "url(images/TreasureInverted.png)")
+    })
+    $('.first_game').mouseleave(function() {
+    $('.first_game').css("background-image", "url(images/Treasure.png)")
+    })
+  })
 
 
-// const button = document.getElementById('readMeButton');
+
+//The following was an attempt to link my user input from the contact page into a Google Sheets file. DID NOT WORK. The script source is from https://medium.com/geekculture/link-your-html-form-to-a-spreadsheet-via-google-forms-9024f0611d82
 //
-// button.addEventListener("click", moreInfo)
+//     function postToGoogle() {
+//       var field1 = $("#firstName").val();
+//       var field2 = $("#lastName").val();
+//       var field3 = $("#email").val();
+//       var field4 = $("#comment").val();
 //
-// const moreInfo = () => {
-//   document.getElementById('readMe').innertext= "testing";
-// }
+//       $.ajax({
+//         url: "{https://docs.google.com/forms/u/0/d/e/1FAIpQLSevE8TqmUw7_aO5gn_gRzvH7qRX7IRg8raybaaf2mnps6u-8Q/formResponse}",
 //
-// moreInfo()
-
-
-// const readButton = document.getElementById('readMeButton');
+//         data: {
+//           "entry.847406057": field1,
+//           "entry.1603159707": field2,
+//           "entry.1613149320": field3,
+//           "entry.910551304": field4
+//         },
+//         type: "POST",
+//         dataType: "xml",
+//         success: function (d) {
+//           $('#contact').trigger('reset');
+//         },
+//         error: function (x, y, z) {
+//           $('#contact').trigger('reset');
+//         }
+//       });
+//       return false;
+//     }
 //
-// readButton.addEventListener("click", moreInfo);
 //
-// const moreInfo = () => {
-//   document.getElementById('readMe').innerHTML= "testing";
-// }
-
-// let button = document.querySelector('#readMeButton');
-// let message = document.querySelector('#readMe');
 //
-// button.addEventListener('click', () => {
-//   message.classList.toggle('reveal');
-// })
-
-
-
-
-// The below code was an attempt to get the dropdown menus via jQuery, but I ended up getting it to work with CSS...later I realized I had not synced my script sources for javascript and jQuery with my html files, and that may have been why the below attemps didn't work:
-
-
-// $(() => {
-//   $('.about').on('click', () => {
-//     $('.aboutUL').attr("display", "")
-//     $('.aboutUL').attr("pointer-events", "all")
-//     $('.aboutUL').attr("transform", "translateY(0px)")
-//   }
-// )})
-
+//
+//
+//
+//
+//
+//
+//
+//
+// // The below code was an attempt to get the dropdown menus via jQuery, but I ended up getting it to work with CSS...later I realized I had not synced my script sources for javascript and jQuery with my html files, and that may have been why the below attemps didn't work:
 // //
-// $(() => {
-//     $('.about').hover(function(){
-//       $('.aboutUL',this).slideDown(500)
-//       }
-//       function(){
-//       $('.aboutUL',this).slideUp(300)
-//       }
-//   })
-
-  //
-  // $(() => {
-  //   if $('.about').on('click') {
-  //     $('.aboutUL').show('slow')
-  //   }
-  //   else {
-  //     $('.aboutUL').hide('slow')
-  //   }
-  // )})
+// //
+// // $(() => {
+// //   $('.about').on('click', () => {
+// //     $('.aboutUL').attr("display", "")
+// //     $('.aboutUL').attr("pointer-events", "all")
+// //     $('.aboutUL').attr("transform", "translateY(0px)")
+// //   }
+// // )})
+//
+// // //
+// // $(() => {
+// //     $('.about').hover(function(){
+// //       $('.aboutUL',this).slideDown(500)
+// //       }
+// //       function(){
+// //       $('.aboutUL',this).slideUp(300)
+// //       }
+// //   })
+//
+//   //
+//   // $(() => {
+//   //   if $('.about').on('click') {
+//   //     $('.aboutUL').show('slow')
+//   //   }
+//   //   else {
+//   //     $('.aboutUL').hide('slow')
+//   //   }
+//   // )})
